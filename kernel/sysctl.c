96a8e1caae2d1f335101a56b0fdbe4616b7e0829
Serge Hallyn <serge.hallyn@canonical.com>
2013-05-31 19:12:12 +0100

    sysctl: add sysctl to disallow unprivileged CLONE_NEWUSER by default
    add sysctl to disallow unprivileged CLONE_NEWUSER by default
    
    This is a short-term patch.  Unprivileged use of CLONE_NEWUSER
    is certainly an intended feature of user namespaces.  However
    for at least saucy we want to make sure that, if any security
    issues are found, we have a fail-safe.
    
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>
    [bwh: Remove unneeded binary sysctl bits]
    [bwh: Keep this sysctl, but change the default to enabled]
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -34,6 +34,10 @@ EXPORT_SYMBOL_GPL(sysctl_long_vals);
 
 extern int sysctl_sched_yield_type;
 
+#ifdef CONFIG_USER_NS
+extern int unprivileged_userns_clone;
+#endif
+
 /* Constants used for minimum and maximum */
 static const int ngroups_max = NGROUPS_MAX;
 static const int cap_last_cap = CAP_LAST_CAP;
@@ -1466,6 +1470,15 @@ static const struct ctl_table sysctl_subsys_table[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_TWO,
 	},
+#ifdef CONFIG_USER_NS
+	{
+		.procname	= "unprivileged_userns_clone",
+		.data		= &unprivileged_userns_clone,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+#endif
 #ifdef CONFIG_PROC_SYSCTL
 	{
 		.procname	= "sysctl_writes_strict",
