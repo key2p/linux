ba31a1d886e97110e35fef3b6648a24f09d56775
aosemp <aosemp@yizhisec.com>
2025-12-04 10:17:42 +0000

    APOD CI: add ci build apod iso&zip

--- ci/build_xanmod_docker.sh
+++ ci/build_xanmod_docker.sh
@@ -13,7 +13,10 @@ export PATH="/usr/lib/llvm-21/bin/:$PATH"
 
 # config apt llvm
 sed -i '/llvm-toolchain/d' /etc/apt/sources.list
-echo "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-21 main" >> /etc/apt/sources.list
+
+( cat /etc/os-release | grep jammy ) && (echo "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-21 main" >> /etc/apt/sources.list)
+( cat /etc/os-release | grep noble ) && (echo "deb http://apt.llvm.org/noble/ llvm-toolchain-noble-21 main" >> /etc/apt/sources.list)
+( cat /etc/os-release | grep oracular ) && (echo "deb http://apt.llvm.org/oracular/ llvm-toolchain-oracular-21 main" >> /etc/apt/sources.list)
 
 mkdir -p /etc/apt/trusted.gpg.d/ || true
 curl -L https://apt.llvm.org/llvm-snapshot.gpg.key -o /etc/apt/trusted.gpg.d/apt.llvm.org.asc
@@ -25,7 +28,8 @@ apt update -y
 apt install -y --no-install-suggests --no-install-recommends curl libc6 libgcc-s1 libicu-dev liblzma5 libstdc++6 libxml2 libzstd1 zlib1g xz-utils \
   fakeroot build-essential git wget openssl ca-certificates libncurses-dev zstd xz-utils flex debhelper rsync kmod cpio gpg pahole python3 \
   pkgconf libdwarf-dev libdw-dev systemtap-sdt-dev libunwind-dev python3-dev libzstd-dev libcap-dev libnuma-dev libtraceevent-dev uuid-dev libpfm4-dev libbfd-dev libbabeltrace-dev libperl-dev libpci-dev libpcap-dev rpm \
-  debhelper debhelper-compat=12 bc bison flex libdw-dev libelf-dev libssl-dev llvm-21 clang-21 lld-21
+  debhelper debhelper-compat=12 bc bison flex libdw-dev libelf-dev libssl-dev llvm-21 clang-21 lld-21 \
+  curl tar gzip xz-utils dpkg parted fdisk util-linux dosfstools e2fsprogs xorriso grub-pc-bin grub-efi-amd64-bin zerofree mtools upx 
 
 apt-get clean
 which llc || true
